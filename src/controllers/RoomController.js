e926022968f803ccb9e4bf68691ee516115e3e1c
Lucas Buzo <lucabtei@outlook.com>
2021-06-25 20:22:41 -0300

    sqlite

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,11 +1,28 @@
-module.exports = {
-    create(req, res) {
-        let roomId = 1234567
+const Database = require('../db/config')
 
-        createInfoBD = {
-            
+module.exports = {
+    async create(req, res) {
+        const db = await Database()
+        const pass = req.body.password
+
+        let roomId
+
+        for(i = 0; i < 6; i++) {
+            i == 0 ?  roomId = Math.floor(Math.random() * 10).toString() : 
+            roomId = roomId + Math.floor(Math.random() * 10).toString()
         }
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        )VALUES(
+            ${parseInt(roomId)},
+            ${pass}
+        )`)
+
+        await db.close()
 
         res.redirect(`/room/${roomId}`)
     }
 }
+
