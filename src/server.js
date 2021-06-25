10532e6c19100d69d430d2e53e2e44cab216c477
Lucas Buzo <lucabtei@outlook.com>
2021-06-24 23:56:25 -0300

    node-config

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const route = require('./route')
+const path = require('path')
+
+// importa o express
+const server = express()
+
+//fala qual vai ser a view engine, no caso Ejs
+server.set('view engine', 'ejs')
+
+//Fala onde está a pasta que será usada para mostrar o layout
+server.use(express.static('public'))
+
+//Altera a pasta para puxar, no caso a pasta views, __dirname pega a pasta onde o arquivo está, nesse caso src, assim ele fica src/views
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(route)
+
+server.listen(8080, () => console.log('Rodando'))
